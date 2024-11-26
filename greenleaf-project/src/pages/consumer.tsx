d2e737ee065d11652610c866cc7f97e2b6ca9612
ILYOSBEK <ilyosbeksalayev240@gmail.com>
2024-11-26 14:44:53 +0500

    green

--- greenleaf-project/src/pages/consumer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Consumer = () => {
-  return (
-    <div>Consumer</div>
-  )
-}
-
-export default Consumer
--- greenleaf-project/src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-export const submitRegistration = async (data: any) => {
-  return fetch("/api/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-};
